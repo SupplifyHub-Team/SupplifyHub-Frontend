d5a982127cfd51a4194b14a6c76d5711d2857ec9
Ahmed Fayyad <ahfayyad.m@gmail.com>
2025-09-25 13:34:00 +0300

    Update Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import LogoImage from "../../public/logov3.png";
+import LogoImage from "../../public/photo_supplifyhup.jpg";
 import { Link } from "@/i18n/navigation";
 
 export default function Logo({ size = 200 }: { size?: number }) {
