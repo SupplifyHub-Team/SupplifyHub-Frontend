ee241e1b963d9f03126c7e76176856af3d51f133
Ahmed Fayyad <ahfayyad.m@gmail.com>
2025-10-19 14:29:57 +0300

    change head hero section

--- src/app/[locale]/(main)/(home)/_components/sections/HeroSection.tsx
+++ src/app/[locale]/(main)/(home)/_components/sections/HeroSection.tsx
@@ -22,7 +22,6 @@ export default function HeroSection() {
               <span className="bg-gradient-to-r from-indigo-600 via-purple-600 to-indigo-600 bg-clip-text text-transparent text-6xl">
                 SUPPLIFYHUB
               </span>
-              <p class="bg-gradient-to-r from-indigo-600 via-purple-600 to-indigo-600 bg-clip-text text-transparent text-4xl py-2">سبليفاي</p>
             </Badge>
           </h1>
 
